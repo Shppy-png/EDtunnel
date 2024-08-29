6a3c4f086c54203c9f67e925d36824f50f593e7a
Shppy-png <kushopee234@gmail.com>
2024-08-29 12:52:31 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '9df74828-1cce-40cd-a5ea-145ec23d19fa';
 
-const พร็อกซีไอพีs = ['138.2.26.192'];
+const พร็อกซีไอพีs = ['8.222.128.32'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
